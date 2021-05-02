6263f826564d7519ab9e04a728928cf3df4c14e9
LennartMac <lennartpopma@hotmail.com>
2021-05-02 14:36:08 +0200

    Logic to place market buy order and limit sell order

--- src/main/java/com/lennart/ElonTweetBot.java
+++ src/main/java/com/lennart/ElonTweetBot.java
@@ -3,6 +3,8 @@ package com.lennart;
 import com.binance.api.client.domain.OrderStatus;
 import com.binance.api.client.domain.account.NewOrderResponse;
 
+import java.text.DecimalFormat;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -12,7 +14,8 @@ public class ElonTweetBot {
 
     private TweetMachine tweetMachine;
     private BinanceMachine binanceMachine;
-    String latestDikkeNiffoTweetWithKeyword;
+    private String latestElonTweetWithKeyword;
+    private long latestTimeOfElonKeywordTweet = 0;
 
     public ElonTweetBot(TweetMachine tweetMachine, BinanceMachine binanceMachine) {
         this.tweetMachine = tweetMachine;
@@ -23,47 +26,64 @@ public class ElonTweetBot {
         ElonTweetBot elonTweetBot = new ElonTweetBot(new TweetMachine(), new BinanceMachine());
 
         while(true) {
-            elonTweetBot.continuousTestDikkeNiffoLogic();
+            elonTweetBot.continuousElonLogic();
             TimeUnit.SECONDS.sleep(1);
-        }
-    }
-
-    private void continuousTestDikkeNiffoLogic() throws Exception {
-        String mostRecentDikkeNiffoTweet = tweetMachine.getMostRecentDikkeNiffoTweet();
-
-        if(mostRecentDikkeNiffoTweet.contains("eije") && !mostRecentDikkeNiffoTweet.equals(latestDikkeNiffoTweetWithKeyword)) {
-            latestDikkeNiffoTweetWithKeyword = mostRecentDikkeNiffoTweet;
-            System.out.println("ELONDOGE -> most recent tweet contains 'eije', placing Binance order");
-            binanceMachine.placeDogeLimitBuyOrder();
         }
     }
 
     private void continuousElonLogic() throws Exception {
         String mostRecentElonTweet = tweetMachine.getMostRecentElonTweet();
 
-        if(elonTweetContainsKeyword(mostRecentElonTweet)) {
-            NewOrderResponse marketOrderResponse = binanceMachine.placeDogeMarketBuyOrder();
+        if(elonTweetContainsKeyword(mostRecentElonTweet) && !mostRecentElonTweet.equals(latestElonTweetWithKeyword)) {
+            if(thereIsSufficientTimeBetweenLastKeywordTweetAndCurrentOne()) {
+                System.out.println("ELONDOGE -> new tweet with keyword identified. Tweet: " + mostRecentElonTweet);
+                System.out.println("ELONDOGE -> placing market buy order");
 
-            while(!marketOrderResponse.getStatus().equals(OrderStatus.FILLED)) {
-                System.out.println("Waiting for fill of market order");
+                latestElonTweetWithKeyword = mostRecentElonTweet;
+                latestTimeOfElonKeywordTweet = new Date().getTime();
+                NewOrderResponse marketOrderResponse = binanceMachine.placeDogeMarketBuyOrder();
+
+                while(!marketOrderResponse.getStatus().equals(OrderStatus.FILLED)) {
+                    System.out.println("ELONDOGE -> waiting for fill of market order");
+                }
+
+                double priceOfMarketOrder = binanceMachine.getPriceOfLastTrade();
+                System.out.println("ELONDOGE -> price of market buy order: " + priceOfMarketOrder);
+                String priceToSell = getPriceToSell(priceOfMarketOrder);
+                System.out.println("ELONDOGE -> price for limit sell order: " + priceToSell);
+                System.out.println("ELONDOGE -> placing limit sell order");
+                binanceMachine.placeDogeLimitSellOrder(String.valueOf(priceToSell));
             }
-
-            double priceOfMarketOrder = Double.valueOf(marketOrderResponse.getPrice());
-            double priceToSell = priceOfMarketOrder * 1.1;
-
-            binanceMachine.placeDogeLimitSellOrder(String.valueOf(priceToSell));
         }
     }
 
     private boolean elonTweetContainsKeyword(String latestElonTweet) {
         String elonTweetToLowerCase = latestElonTweet.toLowerCase();
 
-        if(elonTweetToLowerCase.contains("major")) {
+        if(elonTweetToLowerCase.contains("eije")) {
             return true;
         }
 
         return false;
     }
 
+    private boolean thereIsSufficientTimeBetweenLastKeywordTweetAndCurrentOne() {
+        //todo: check server time
+        //todo: nu gaat ie nog order versturen als tweede doge tweet was 5 min na eerste, en dan zijn er 2 uur verstreken
+        //todo: je moet kijken naar created at property van het tweet item
+        long currentTime = new Date().getTime();
 
+        if(currentTime - latestTimeOfElonKeywordTweet > 7_200_000) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private String getPriceToSell(double priceOfMarketOrder) {
+        double priceToSell = priceOfMarketOrder * 1.1;
+        DecimalFormat formatter = new DecimalFormat("0.00000000");
+        String priceToSellAsString = formatter.format(priceToSell);
+        return priceToSellAsString;
+    }
 }
--- src/main/java/com/lennart/TweetMachine.java
+++ src/main/java/com/lennart/TweetMachine.java
@@ -10,12 +10,6 @@ import java.util.stream.Collectors;
  */
 public class TweetMachine {
 
-    private Twitter twittertje;
-
-    public TweetMachine() {
-        setTwittertje();
-    }
-
     public String getMostRecentElonTweet() throws Exception {
         String consumerKey = "XB2wdvrDkQeyidS5KhNYFp5oP";
         String consumerSecret = "hR7Biwi6B4qNVwtjQlms7BlPu6RvborgYPnh5MPMRs0ApSEFKE";
@@ -34,38 +28,6 @@ public class TweetMachine {
 
         Paging paging = new Paging(1, 1);
 
-        List<String> elonTweets = twitter.getUserTimeline("elonmusk", paging).stream()
-                .map(item -> item.getText())
-                .collect(Collectors.toList());
-
-        String lastElonTweet;
-
-        if(elonTweets.isEmpty()) {
-            lastElonTweet = "NA";
-        } else {
-            lastElonTweet = elonTweets.get(0);
-        }
-
-        return lastElonTweet;
-    }
-
-    public String getMostRecentDikkeNiffoTweet() throws Exception {
-        String consumerKey = "XB2wdvrDkQeyidS5KhNYFp5oP";
-        String consumerSecret = "hR7Biwi6B4qNVwtjQlms7BlPu6RvborgYPnh5MPMRs0ApSEFKE";
-        String accessToken = "1295662434229919744-9faVomqpSw6qI0bPocBGJvCMxOpGHZ";
-        String accessSecret = "oDuujFjYP8vWIVH89AjZ9BFoeYlzw252VQjj1Rd4UmxPg";
-
-        ConfigurationBuilder cb = new ConfigurationBuilder();
-        cb.setDebugEnabled(true)
-                .setOAuthConsumerKey(consumerKey)
-                .setOAuthConsumerSecret(consumerSecret)
-                .setOAuthAccessToken(accessToken)
-                .setOAuthAccessTokenSecret(accessSecret);
-
-        TwitterFactory factory = new TwitterFactory(cb.build());
-        Twitter twitter = factory.getInstance();
-        Paging paging = new Paging(1, 1);
-
         List<String> elonTweets = twitter.getUserTimeline("dikkeniffo", paging).stream()
                 .map(item -> item.getText())
                 .collect(Collectors.toList());
@@ -79,22 +41,5 @@ public class TweetMachine {
         }
 
         return lastElonTweet;
-    }
-
-    private void setTwittertje() {
-        String consumerKey = "XB2wdvrDkQeyidS5KhNYFp5oP";
-        String consumerSecret = "hR7Biwi6B4qNVwtjQlms7BlPu6RvborgYPnh5MPMRs0ApSEFKE";
-        String accessToken = "1295662434229919744-9faVomqpSw6qI0bPocBGJvCMxOpGHZ";
-        String accessSecret = "oDuujFjYP8vWIVH89AjZ9BFoeYlzw252VQjj1Rd4UmxPg";
-
-        ConfigurationBuilder cb = new ConfigurationBuilder();
-        cb.setDebugEnabled(true)
-                .setOAuthConsumerKey(consumerKey)
-                .setOAuthConsumerSecret(consumerSecret)
-                .setOAuthAccessToken(accessToken)
-                .setOAuthAccessTokenSecret(accessSecret);
-
-        TwitterFactory factory = new TwitterFactory(cb.build());
-        twittertje = factory.getInstance();
     }
 }
