3248a9f8e5f9be8a9f4ed452361f36880884af1d
LennartMac <lennartpopma@hotmail.com>
2021-05-04 20:27:26 +0200

    Added first version of Telegram bot

--- src/main/java/com/lennart/ElonTweetBot.java
+++ src/main/java/com/lennart/ElonTweetBot.java
@@ -2,12 +2,24 @@ package com.lennart;
 
 import com.binance.api.client.domain.OrderStatus;
 import com.binance.api.client.domain.account.NewOrderResponse;
+import com.lennart.telegram.MyAmazingBot;
+
+import org.slf4j.LoggerFactory;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import twitter4j.Status;
 
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 
 /**
  * Created by LennartMac on 01/05/2021.
@@ -25,6 +37,21 @@ public class ElonTweetBot {
     }
 
     public static void main(String[] args) throws Exception {
+        Set<String> loggers = new HashSet<>(Arrays.asList("org.apache.http", "groovyx.net.http"));
+
+        for(String log:loggers) {
+            Logger logger = (Logger) LoggerFactory.getLogger(log);
+            logger.setLevel(Level.WARN);
+            logger.setAdditive(false);
+        }
+
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new MyAmazingBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
         ElonTweetBot elonTweetBot = new ElonTweetBot(new TweetMachine(), new BinanceMachine());
         int counter = 0;
 
---
+++ src/main/java/com/lennart/telegram/MyAmazingBot.java
@@ -0,0 +1,80 @@
+package com.lennart.telegram;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class MyAmazingBot extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotToken() {
+        return "1643465373:AAHhn9EfoGpccf48n3w5FaIRhwJ04yPDsPk";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if(update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+
+            String botReturnMessageText = null;
+
+            if(messageText.toLowerCase().contains("eije") || messageText.toLowerCase().contains("leon")) {
+                if(Math.random() < 0.2) {
+                    botReturnMessageText = "EIJE!";
+                } else if(Math.random() < 0.4) {
+                    botReturnMessageText = "HMM!";
+                } else if(Math.random() < 0.6) {
+                    botReturnMessageText = "WATTE!";
+                } else if(Math.random() < 0.8) {
+                    botReturnMessageText = "Tzeije?!";
+                } else {
+                    botReturnMessageText = "HMM?!?!";
+                }
+            } else {
+                if(Math.random() < 0.01) {
+                    double secondRandom = Math.random();
+
+                    if(secondRandom < 0.1) {
+                        botReturnMessageText = "UCHE UCHE!";
+                    } else if(secondRandom < 0.2) {
+                        botReturnMessageText = "HOU OP!";
+                    } else if(secondRandom < 0.3) {
+                        botReturnMessageText = "Ik meen het!";
+                    } else if(secondRandom < 0.4) {
+                        botReturnMessageText = "Nee... echt even ophouden nou";
+                    } else if(secondRandom < 0.5) {
+                        botReturnMessageText = "NEE IK MEEN HET!";
+                    } else if(secondRandom < 0.6) {
+                        botReturnMessageText = "Das ook zoiets...";
+                    } else if(secondRandom < 0.7) {
+                        botReturnMessageText = "Tuf tuf!";
+                    } else if(secondRandom < 0.8) {
+                        botReturnMessageText = "HatsjieeHatsjiee!";
+                    } else if(secondRandom < 0.9) {
+                        botReturnMessageText = "Oh boy.. weet iemand de Telegram username van Suuske?";
+                    } else {
+                        botReturnMessageText = "NEE OPHOUDEN!";
+                    }
+                }
+            }
+
+            if(botReturnMessageText != null) {
+                SendMessage message = new SendMessage();
+                message.setChatId(update.getMessage().getChatId().toString());
+                message.setText(botReturnMessageText);
+
+                try {
+                    execute(message);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "eijebot";
+    }
+}
